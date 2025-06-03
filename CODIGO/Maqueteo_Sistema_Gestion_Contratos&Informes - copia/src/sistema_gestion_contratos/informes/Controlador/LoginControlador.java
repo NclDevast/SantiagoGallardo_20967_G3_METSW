c438bb881cd06806dde4d0abeb585940747406b8
Isabela2701 <isabela2701love@gmail.com>
2025-06-03 14:56:37 -0500

    iniciado maqueteo de gestion de contratos

---
+++ CODIGO/Maqueteo_Sistema_Gestion_Contratos&Informes - copia/src/sistema_gestion_contratos/informes/Controlador/LoginControlador.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistema_gestion_contratos.informes.Controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+import sistema_gestion_contratos.informes.Modelo.Usuario;
+import sistema_gestion_contratos.informes.Vista.*;
+
+/**
+ *
+ * @author Isabela
+ */
+public class LoginControlador implements ActionListener{
+    private VISTA_VALIDACION vista_validacion;
+    private Programa programa;
+    private Usuario modelo;
+    private Boolean LoginEstado;
+    
+    public LoginControlador (VISTA_VALIDACION vista, Usuario modelo, Programa programa){
+        this.vista_validacion=vista;
+        this.modelo=modelo;
+        this.vista_validacion.btnLogin.addActionListener(this);
+        this.programa = programa;
+    }
+    public void iniciar()
+    {
+        vista_validacion.setTitle("Validación de usuario");
+        vista_validacion.setLocationRelativeTo(null);
+    }
+    public void actionPerformed(ActionEvent e){
+        modelo.setNombreUsuario(vista_validacion.txtUsuario.getText());
+        modelo.setContraseña(vista_validacion.txtContrasena.getText());
+        this.LoginEstado = modelo.ValidarUsuarios();
+        if(!this.LoginEstado){
+        JOptionPane.showMessageDialog(null,"usuario o contraseña no valido");
+        vista_validacion.txtContrasena.setText("");
+        vista_validacion.txtUsuario.setText("");
+        vista_validacion.txtUsuario.requestFocusInWindow();
+        }
+        else {
+        programa.setVisible(true);
+        programa.setLocationRelativeTo(null);
+        }
+        
+    }
+    
+}
---
+++ CODIGO/Maqueteo_Sistema_Gestion_Contratos&Informes - copia/src/sistema_gestion_contratos/informes/Sistema_Gestion_ContratosInformes.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistema_gestion_contratos.informes;
+
+import sistema_gestion_contratos.informes.Controlador.LoginControlador;
+import sistema_gestion_contratos.informes.Modelo.Usuario;
+import sistema_gestion_contratos.informes.Vista.*;
+
+/**
+ *
+ * @author Isabela
+ */
+public class Sistema_Gestion_ContratosInformes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VISTA_VALIDACION vista1 =new VISTA_VALIDACION();
+        Programa programa = new Programa();    
+        Usuario mod= new Usuario();
+        vista1.setVisible(true);
+        vista1.setLocationRelativeTo(null);
+        LoginControlador ctrl= new LoginControlador(vista1,mod,programa);
+        ctrl.iniciar();
+
+    }
+    
+}
