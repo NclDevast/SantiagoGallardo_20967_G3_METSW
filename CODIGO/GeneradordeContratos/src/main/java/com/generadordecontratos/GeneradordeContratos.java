34e1f01b2a2eb58b3425f15a3a7c26a26d206cbc
DavidIsmaelPD <davdisma@gmail.com>
2025-06-05 16:28:38 -0500

    Maqueteo_Generador_contratos

---
+++ CODIGO/GeneradordeContratos/src/main/java/com/generadordecontratos/GeneradordeContratos.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.generadordecontratos;
+
+/**
+ *
+ * @author ASUS
+ */
+
+
+public class GeneradordeContratos {
+
+    public static void main(String[] args) {
+        
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new FormularioContrato().setVisible(true);  
+            }
+        });
+    }
+}
+
