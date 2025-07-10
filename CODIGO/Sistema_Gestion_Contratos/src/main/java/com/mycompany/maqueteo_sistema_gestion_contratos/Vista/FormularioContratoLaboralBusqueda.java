104d21ee7fc8705763438eba8302b1c443113248
Isabela2701 <isabela2701love@gmail.com>
2025-07-10 18:46:05 -0500

    Retorno

--- CODIGO/Sistema_Gestion_Contratos/src/main/java/com/mycompany/maqueteo_sistema_gestion_contratos/Vista/FormularioContratoLaboralBusqueda.java
+++ CODIGO/Sistema_Gestion_Contratos/src/main/java/com/mycompany/maqueteo_sistema_gestion_contratos/Vista/FormularioContratoLaboralBusqueda.java
@@ -281,7 +281,7 @@ public class FormularioContratoLaboralBusqueda extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         this.dispose();
-        System.exit(0);
+
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
